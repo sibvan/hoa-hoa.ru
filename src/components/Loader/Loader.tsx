b982a060e96fb34d7517327751ef7f3e591eee59
sibvan <sibvan.dev@gmail.com>
2025-10-16 03:26:15 +0400

    add new loading animation

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,21 +1,39 @@
 import "./Loader.scss";
 import clsx from "clsx";
-import alice from "../../assets/loader/alice.png";
-import face from "../../assets/loader/face.png";
-import arrows from "../../assets/loader/arrows.png";
+import face from "../../assets/loader/face.webp";
+import alice from "../../assets/loader/alice.webp";
+import hand1 from "../../assets/loader/hand-1.webp";
+import hand2 from "../../assets/loader/hand-2.webp";
 
 function Loader({ error }: { error: boolean }) {
   const imgSrc = error ? alice : face;
   const rootDivClasses = clsx("loader", error && "loader_error");
   const h1Classes = clsx("loader__title", error && "loader__title_error");
   const pClasses = clsx("loader__text", error && "loader__text_error");
+  const imgClasses = clsx(
+    "loader__clock-face",
+    error && "loader__clock-face_rotate"
+  );
 
   return (
     <div className={rootDivClasses}>
       <div className="loader__clock">
-        {!error && <img className="loader__clock-arrows" src={arrows} alt="" />}
+        {!error && (
+          <>
+            <img
+              className="loader__clock-hand loader__clock-hand_big"
+              src={hand1}
+              alt=""
+            />
+            <img
+              className="loader__clock-hand loader__clock-hand_small"
+              src={hand2}
+              alt=""
+            />
+          </>
+        )}
 
-        <img className="loader__clock-face" src={imgSrc} alt="" />
+        <img className={imgClasses} src={imgSrc} alt="" />
       </div>
       <div className="loader__text">
         <h1 className={h1Classes}>{error ? "Ошибка" : "Загрузка"}</h1>
