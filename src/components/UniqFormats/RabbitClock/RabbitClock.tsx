131bf621aa73886159495ea671548eb1a66c9346
sibvan <sibvan.dev@gmail.com>
2025-10-16 02:31:02 +0400

    add new clock animation

--- src/components/UniqFormats/RabbitClock/RabbitClock.tsx
+++ src/components/UniqFormats/RabbitClock/RabbitClock.tsx
@@ -1,14 +1,17 @@
 import "./RabbitClock.scss";
-import circle from "../../../assets/uniq-formats/clock/circle.png";
-import clock from "../../../assets/uniq-formats/clock/clock.jpg";
+
+import face from "../../../assets/uniq-formats/clock/face.webp";
+import hand1 from "../../../assets/uniq-formats/clock/hand-1.webp";
+import hand2 from "../../../assets/uniq-formats/clock/hand-2.webp";
 
 function RabbitClock() {
-  return <div className="chapter__format rabbit-clock">
-
-<img className="rabbit-clock__circle" src={circle} alt="" />
-<img className="rabbit-clock__clock" src={clock} alt="" />
-
-  </div>;
+  return (
+    <div className="chapter__format rabbit-clock">
+      <img className="rabbit-clock__hand rabbit-clock__hand_big" src={hand1} alt="" />
+      <img className="rabbit-clock__hand rabbit-clock__hand_small" src={hand2} alt="" />
+      <img className="rabbit-clock__face" src={face} alt="" />
+    </div>
+  );
 }
 
 export default RabbitClock;
